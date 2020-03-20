9c8d2b7ff1700d653992ec8a5160bdfa2dfebe48
quyili <quyli@mail2.sysu.edu.cn>
2020-03-20 12:43:34 +0800

    Default Changelist

--- open_code/X-RAY/Ours/GET_S_AND_MASK/gen_s_and_m_from_noise.py
+++ open_code/X-RAY/Ours/GET_S_AND_MASK/gen_s_and_m_from_noise.py
@@ -9,23 +9,17 @@ from scipy.stats import norm
 
 FLAGS = tf.flags.FLAGS
 
-tf.flags.DEFINE_string('savefile', None, 'Checkpoint save dir')
+tf.flags.DEFINE_string('savefile', None, 'Checkpoint save dir, default: None')
 tf.flags.DEFINE_integer('log_level', 10, 'CRITICAL = 50,ERROR = 40,WARNING = 30,INFO = 20,DEBUG = 10,NOTSET = 0')
-tf.flags.DEFINE_string('load_model', "20190719-1738",
-                       'folder of saved model that you wish to continue training (e.g. 20170602-1936), default: None')
+tf.flags.DEFINE_string('load_model', "20190719-1738",'e.g. 20170602-1936, default: None')
 tf.flags.DEFINE_string('checkpoint', None, "default: None")
-tf.flags.DEFINE_string('code_tensor_name', "GPU_0/random_normal_1:0", "default: None")
-tf.flags.DEFINE_string('f_tensor_name', "GPU_0/Reshape_4:0", "default: None")
-tf.flags.DEFINE_string('m_tensor_name', "GPU_0/Reshape_5:0", "default: None")
-tf.flags.DEFINE_string('j_f_tensor_name', "GPU_3/D_F_1/conv5/conv5/BiasAdd:0", "default: None")
-tf.flags.DEFINE_integer('epoch_steps', 1650, ' default: 15070')
-tf.flags.DEFINE_integer('epochs', 1, ' default: 1')
-tf.flags.DEFINE_float('min_j_f', 0.6, 'default: 0.6')
-tf.flags.DEFINE_float('max_count', 50, 'default: 50')
-tf.flags.DEFINE_float('mae', 0.05, 'default: 0.05')
+tf.flags.DEFINE_string('code_f_g', "GPU_0/random_normal_1:0", "default: None")
+tf.flags.DEFINE_string('s_g', "GPU_0/Reshape_4:0", "default: None")
+tf.flags.DEFINE_string('m_g', "GPU_0/Reshape_5:0", "default: None")
+tf.flags.DEFINE_integer('num', 2000, ' default: 2000')
 
 
-def get_mask_from_f(imgfile):
+def get_mask_from_s(imgfile):
     img = cv2.imread(imgfile, cv2.IMREAD_GRAYSCALE)
     gray = cv2.GaussianBlur(img, (3, 3), 0)
     ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY)
@@ -46,8 +40,7 @@ def train():
         print("<load_model> is None.")
         return
     try:
-        os.makedirs("./test_images/Temp")
-        os.makedirs("./test_images/F")
+        os.makedirs("./test_images/S")
         os.makedirs("./test_images/M")
     except os.error:
         pass
@@ -58,63 +51,28 @@ def train():
     saver = tf.train.import_meta_graph(meta_graph_path)
 
     graph = tf.get_default_graph()
-    code_rm = tf.get_default_graph().get_tensor_by_name(FLAGS.code_tensor_name)
-    f_rm = tf.get_default_graph().get_tensor_by_name(FLAGS.f_tensor_name)
-    mask_rm = tf.get_default_graph().get_tensor_by_name(FLAGS.m_tensor_name)
-    j_f_rm = tf.get_default_graph().get_tensor_by_name(FLAGS.j_f_tensor_name)
+    code_f_g = tf.get_default_graph().get_tensor_by_name(FLAGS.code_f_g)
+    s_g = tf.get_default_graph().get_tensor_by_name(FLAGS.s_g)
+    m_g = tf.get_default_graph().get_tensor_by_name(FLAGS.m_g)
 
     with tf.Session(graph=graph, config=tf.ConfigProto(allow_soft_placement=True)) as sess:
         saver.restore(sess, latest_checkpoint)
         index = 0
-        while index <= FLAGS.epoch_steps * FLAGS.epochs:
+        while index <= FLAGS.num:
             print("image gen start:" + str(index))
+            code_f = sess.run(code_f_g)
+            s, m = sess.run([s_g, m_g], feed_dict={code_f_g: code_f})
 
-            count = 0
-            best_j_f = -1000.0
-            while True:
-                code = sess.run(code_rm)
-                f, m, j_f = sess.run([f_rm, mask_rm, j_f_rm], feed_dict={code_rm: code})
-                j_f = np.mean(np.asarray(j_f))
-                print(count, "j_f: ", j_f)
-
-                if j_f >= FLAGS.min_j_f: break
-
-                jpg_f = np.concatenate([np.asarray(f)[0, :, :, 0:1] * 255, np.asarray(f)[0, :, :, 0:1] * 255,
-                                        np.asarray(f)[0, :, :, 0:1] * 255], axis=-1)
-                cv2.imwrite("./test_images/Temp/f_" + str(index) + "_" + str(count) + ".jpg", jpg_f)
-
-                m_arr_1 = get_mask_from_f("./test_images/Temp/f_" + str(index) + "_" + str(count) + ".jpg")
-                m_arr_2 = np.asarray(m)[0, :, :, 0].astype('float32')
-
-                mae = np.mean(np.abs(m_arr_1 - m_arr_2))
-                print(count, "mae: ", mae)
-
-                if mae <= FLAGS.mae: break
-
-                if j_f > best_j_f:
-                    best_j_f = j_f
-                    best_f = f
-                    best_m = m
-
-                if count >= FLAGS.max_count:
-                    f = best_f
-                    m = best_m
-                    break
-
-                count = count + 1
-
-            SimpleITK.WriteImage(SimpleITK.GetImageFromArray(np.asarray(f)[0, :, :, 0]),
-                                 "./test_images/F/" + str(index) + ".tiff")
+            SimpleITK.WriteImage(SimpleITK.GetImageFromArray(np.asarray(s)[0, :, :, 0]),
+                                 "./test_images/S/" + str(index) + ".tiff")
             SimpleITK.WriteImage(SimpleITK.GetImageFromArray(np.asarray(m)[0, :, :, 0]),
                                  "./test_images/M/" + str(index) + ".tiff")
             print("image gen end:" + str(index))
-
             index += 1
 
 
 def main(unused_argv):
     train()
-    os.system("rm -r " + "./test_images/Temp")
 
 
 if __name__ == '__main__':
--- open_code/X-RAY/Ours/GET_S_AND_MASK/model.py
+++ open_code/X-RAY/Ours/GET_S_AND_MASK/model.py
@@ -27,96 +27,92 @@ class VAE_GAN:
         self.ones = tf.ones(self.input_shape, name="ones")
         self.tenaor_name = {}
 
-        self.EC_F = VEncoder('EC_F', ngf=ngf, units=units, keep_prob=0.85)
-        self.DC_F = VDecoder('DC_F', ngf=ngf, output_channl=2, units=units)
+        self.EC_S = VEncoder('EC_S', ngf=ngf, units=units, keep_prob=0.85)
+        self.DC_S = VDecoder('DC_S', ngf=ngf, output_channl=2, units=units)
 
         self.G_M = Unet('G_M', ngf=ngf / 2, keep_prob=0.9, output_channl=2)
 
-        self.D_F = Discriminator('D_F', ngf=ngf, keep_prob=0.85)
-        self.FD_F = FeatureDiscriminator('FD_F', ngf=ngf)
+        self.D_S = Discriminator('D_S', ngf=ngf, keep_prob=0.85)
+        self.FD_Z = FeatureDiscriminator('FD_Z', ngf=ngf)
 
-    def model(self, f, mask):
-        # F -> F_R VAE
-        f_one_hot = tf.reshape(tf.one_hot(tf.cast(f, dtype=tf.int32), depth=2, axis=-1),
+    def model(self, s, m):
+        s_one_hot = tf.reshape(tf.one_hot(tf.cast(s, dtype=tf.int32), depth=2, axis=-1),
                                shape=[self.input_shape[0], self.input_shape[1], self.input_shape[2],
                                       2 * self.input_shape[3]])
-        m_one_hot = tf.reshape(tf.one_hot(tf.cast(mask, dtype=tf.int32), depth=2, axis=-1),
+        m_one_hot = tf.reshape(tf.one_hot(tf.cast(m, dtype=tf.int32), depth=2, axis=-1),
                                shape=[self.input_shape[0], self.input_shape[1], self.input_shape[2],
                                       2 * self.input_shape[3]])
 
-        code_f_mean, code_f_logvar = self.EC_F(f * 0.1 + 0.9)
+        # VAE
+        code_f_mean, code_f_logvar = self.EC_S(s * 0.1 + 0.9)
         shape = code_f_logvar.get_shape().as_list()
         code_f_std = tf.exp(0.5 * code_f_logvar)
         code_f_epsilon = tf.random_normal(shape, mean=0., stddev=1., dtype=tf.float32)
         code_f = code_f_mean + code_f_std * code_f_epsilon
-        f_r_prob = self.DC_F(code_f)
+        s_r_prob = self.DC_S(code_f)
 
-        # CODE_F_RM
-        code_f_rm = tf.random_normal(shape, mean=0., stddev=1., dtype=tf.float32)
-        f_rm_prob = self.DC_F(code_f_rm)
+        # CODE_S_RM
+        code_f_g = tf.random_normal(shape, mean=0., stddev=1., dtype=tf.float32)
+        s_g_prob = self.DC_S(code_f_g)
 
         # D,FD
-        j_f = self.D_F(f_one_hot)
-        j_f_rm = self.D_F(f_rm_prob)
+        j_s = self.D_S(s_one_hot)
+        j_s_g = self.D_S(s_g_prob)
 
         code_f = tf.reshape(code_f, shape=[self.input_shape[0], 64, 64, -1])
-        code_f_rm = tf.reshape(code_f_rm, shape=[self.input_shape[0], 64, 64, -1])
-        j_code_f_rm = self.FD_F(code_f_rm)
-        j_code_f = self.FD_F(code_f)
+        code_f_g = tf.reshape(code_f_g, shape=[self.input_shape[0], 64, 64, -1])
+        j_code_f_g = self.FD_Z(code_f_g)
+        j_code_f = self.FD_Z(code_f)
 
-        mask_r_prob = self.G_M(f_one_hot)
-        mask_rm_prob = self.G_M(f_rm_prob)
+        m_r_prob = self.G_M(s_one_hot)
+        m_g_prob = self.G_M(s_g_prob)
 
         D_loss = 0.0
-        FG_loss = 0.0
-        MG_loss = 0.0
-        D_loss += self.mse_loss(j_code_f_rm, 1.0) * 10
+        GS_loss = 0.0
+        GM_loss = 0.0
+        D_loss += self.mse_loss(j_code_f_g, 1.0) * 10
         D_loss += self.mse_loss(j_code_f, 0.0) * 10
-        FG_loss += self.mse_loss(j_code_f, 1.0) * 0.001
+        GS_loss += self.mse_loss(j_code_f, 1.0) * 0.001
 
-        FG_loss += self.mse_loss(tf.reduce_mean(code_f_mean), 0.0) * 0.001
-        FG_loss += self.mse_loss(tf.reduce_mean(code_f_std), 1.0) * 0.001
+        GS_loss += self.mse_loss(tf.reduce_mean(code_f_mean), 0.0) * 0.001
+        GS_loss += self.mse_loss(tf.reduce_mean(code_f_std), 1.0) * 0.001
 
-        D_loss += self.mse_loss(j_f, 1.0) * 25
-        D_loss += self.mse_loss(j_f_rm, 0.0) * 25
-        FG_loss += self.mse_loss(j_f_rm, 1.0) * 0.1
+        D_loss += self.mse_loss(j_s, 1.0) * 25
+        D_loss += self.mse_loss(j_s_g, 0.0) * 25
+        GS_loss += self.mse_loss(j_s_g, 1.0) * 0.1
 
-        FG_loss += self.mse_loss(f_one_hot, f_r_prob) * 25
+        GS_loss += self.mse_loss(s_one_hot, s_r_prob) * 25
 
-        FG_loss += tf.reduce_mean(tf.abs(f_one_hot - f_r_prob)) * 10
-        FG_loss += (tf.reduce_mean(f_r_prob[:, :, :, 0]) - tf.reduce_mean(f_r_prob[:, :, :, 1])) * 0.0001
-        FG_loss += (tf.reduce_mean(f_rm_prob[:, :, :, 0]) - tf.reduce_mean(f_rm_prob[:, :, :, 1])) * 0.0001
+        GS_loss += tf.reduce_mean(tf.abs(s_one_hot - s_r_prob)) * 10
+        GS_loss += (tf.reduce_mean(s_r_prob[:, :, :, 0]) - tf.reduce_mean(s_r_prob[:, :, :, 1])) * 0.0001
+        GS_loss += (tf.reduce_mean(s_g_prob[:, :, :, 0]) - tf.reduce_mean(s_g_prob[:, :, :, 1])) * 0.0001
 
-        FG_loss += self.mse_loss(0.0, m_one_hot * f_r_prob) * 1
-        FG_loss += self.mse_loss(0.0, mask_rm_prob * f_rm_prob) * 1
+        GS_loss += self.mse_loss(0.0, m_one_hot * s_r_prob) * 1
+        GS_loss += self.mse_loss(0.0, m_g_prob * s_g_prob) * 1
 
-        MG_loss += self.mse_loss(m_one_hot, mask_r_prob) * 15
+        GM_loss += self.mse_loss(m_one_hot, m_r_prob) * 15
 
-        new_f = tf.reshape(tf.cast(tf.argmax(f_one_hot, axis=-1), dtype=tf.float32), shape=self.input_shape)
-        f_r = tf.reshape(tf.cast(tf.argmax(f_r_prob, axis=-1), dtype=tf.float32), shape=self.input_shape)
-        f_rm = tf.reshape(tf.cast(tf.argmax(f_rm_prob, axis=-1), dtype=tf.float32), shape=self.input_shape)
-        mask_r = tf.reshape(tf.cast(tf.argmax(mask_r_prob, axis=-1), dtype=tf.float32), shape=self.input_shape)
-        mask_rm = tf.reshape(tf.cast(tf.argmax(mask_rm_prob, axis=-1), dtype=tf.float32), shape=self.input_shape)
+        s_r = tf.reshape(tf.cast(tf.argmax(s_r_prob, axis=-1), dtype=tf.float32), shape=self.input_shape)
+        s_g = tf.reshape(tf.cast(tf.argmax(s_g_prob, axis=-1), dtype=tf.float32), shape=self.input_shape)
+        m_r = tf.reshape(tf.cast(tf.argmax(m_r_prob, axis=-1), dtype=tf.float32), shape=self.input_shape)
+        m_g = tf.reshape(tf.cast(tf.argmax(m_g_prob, axis=-1), dtype=tf.float32), shape=self.input_shape)
+        self.tenaor_name["code_f_g"] = str(code_f_g)
+        self.tenaor_name["s_g"] = str(s_g)
+        self.tenaor_name["m_g"] = str(m_g)
+        self.tenaor_name["j_s_g"] = str(j_s_g)
 
-        self.tenaor_name["code_f_rm"] = str(code_f_rm)
-        self.tenaor_name["f_rm"] = str(f_rm)
-        self.tenaor_name["j_f_rm"] = str(j_f_rm)
+        image_list = [s, s_r, s_g, m, m_r, m_g]
+        j_list = [j_code_f, j_code_f_g, j_s, j_s_g]
+        loss_list = [GS_loss + GM_loss, D_loss]
 
-        image_list = [new_f, f_r, f_rm, mask, mask_r, mask_rm]
-        code_list = [code_f, code_f_rm]
-        j_list = [j_code_f, j_code_f_rm, j_f, j_f_rm]
-        loss_list = [FG_loss + MG_loss, D_loss]
-
-        return image_list, code_list, j_list, loss_list
+        return loss_list,image_list, j_list,
 
     def get_variables(self):
-        return [self.EC_F.variables
-                + self.DC_F.variables
-                + self.G_M.variables
-            ,
-                self.D_F.variables +
-                self.FD_F.variables
-                ]
+        return [self.EC_S.variables
+                + self.DC_S.variables
+                + self.G_M.variables ,
+                self.D_S.variables +
+                self.FD_Z.variables]
 
     def optimize(self):
         def make_optimizer(name='Adam'):
@@ -129,52 +125,6 @@ class VAE_GAN:
         D_optimizer = make_optimizer(name='Adam_D')
 
         return G_optimizer, D_optimizer
-
-    def evaluation_code(self, code_list):
-        code_f, code_f_rm = \
-            code_list[0], code_list[1]
-        list = [self.PSNR(code_f, code_f_rm)]
-        return list
-
-    def evaluation_code_summary(self, evluation_list):
-        tf.summary.scalar('evaluation_code/PSNR/code_f__VS__code_f_rm', evluation_list[0])
-
-    def evaluation(self, image_list):
-        f, f_r, f_rm, mask, mask_r, mask_rm = image_list[0], image_list[1], image_list[2], image_list[3], image_list[4], \
-                                              image_list[5]
-        list = [self.PSNR(f, f_r),
-                self.SSIM(f, f_r),
-                self.PSNR(mask, mask_r),
-                self.SSIM(mask, mask_r)]
-        return list
-
-    def evaluation_summary(self, evluation_list):
-        tf.summary.scalar('evaluation/PSNR/f__VS__f_r', evluation_list[0])
-        tf.summary.scalar('evaluation/SSIM/f__VS__f_r', evluation_list[1])
-        tf.summary.scalar('evaluation/PSNR/mask__VS__mask_r', evluation_list[2])
-        tf.summary.scalar('evaluation/SSIM/mask__VS__mask_r', evluation_list[3])
-
-    def histogram_summary(self, j_list):
-        j_code_f, j_code_f_rm, j_f, j_f_rm = j_list[0], j_list[1], j_list[2], j_list[3]
-        tf.summary.histogram('discriminator/TRUE/j_code_f_rm', j_code_f_rm)
-        tf.summary.histogram('discriminator/FALSE/j_code_f', j_code_f)
-        tf.summary.histogram('discriminator/TRUE/j_f', j_f)
-        tf.summary.histogram('discriminator/FALSE/j_f_rm', j_f_rm)
-
-    def loss_summary(self, loss_list):
-        G_loss, D_loss = loss_list[0], loss_list[1]
-        tf.summary.scalar('loss/G_loss', G_loss)
-        tf.summary.scalar('loss/D_loss', D_loss)
-
-    def image_summary(self, image_list):
-        f, f_r, f_rm, mask, mask_r, mask_rm = image_list[0], image_list[1], image_list[2], image_list[3], image_list[4], \
-                                              image_list[5]
-        tf.summary.image('image/f', f)
-        tf.summary.image('image/f_rm', f_rm)
-        tf.summary.image('image/f_r', f_r)
-        tf.summary.image('image/mask', mask)
-        tf.summary.image('image/mask_rm', mask_rm)
-        tf.summary.image('image/mask_r', mask_r)
 
     def mse_loss(self, x, y):
         """ supervised loss (L2 norm)
