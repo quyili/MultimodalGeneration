16023725da94bba4e617459a46bdaa6e17ecc3b3
quyili <quyli@mail2.sysu.edu.cn>
2020-03-26 12:43:45 +0800

    Default Changelist

--- code_demo/TC2019/Ours/DETECTION/train.py
+++ code_demo/TC2019/Ours/DETECTION/train.py
@@ -115,20 +115,35 @@ def average_gradients(grads_list):
     return average_grads
 
 
-def check_numerics(input_dataset, message):
-    if str(input_dataset).find('Tensor') == 0:
-        input_dataset = tf.check_numerics(input_dataset, message)
+def voc_ap(rec, prec, use_07_metric=False):
+    """ ap = voc_ap(rec, prec, [use_07_metric])
+    Compute VOC AP given precision and recall.
+    If use_07_metric is true, uses the
+    VOC 07 11 point method (default:False).
+    """
+    if use_07_metric:
+        # 11 point metric
+        ap = 0.
+        for t in np.arange(0., 1.1, 0.1):
+            if np.sum(rec >= t) == 0:
+                p = 0
+            else:
+                p = np.max(prec[rec >= t])
+            ap = ap + p / 11.
     else:
-        dataset = np.array(input_dataset)
-        nan_count = np.count_nonzero(dataset != dataset)
-        inf_count = len(dataset[dataset == float("inf")])
-        n_inf_count = len(dataset[dataset == float("-inf")])
-        if nan_count > 0 or inf_count > 0 or n_inf_count > 0:
-            data_error = '[' + message + ']  [nan：' + str(nan_count) + '|inf：' + str(inf_count) + '|-inf：' + str(
-                n_inf_count) + ']'
-            raise Exception(data_error)
-    return input_dataset
-
+        # correct AP calculation
+        # first append sentinel values at the end
+        mrec = np.concatenate(([0.], rec, [1.]))
+        mpre = np.concatenate(([0.], prec, [0.]))
+        # compute the precision envelope
+        for i in range(mpre.size - 1, 0, -1):
+            mpre[i - 1] = np.maximum(mpre[i - 1], mpre[i])
+        # to calculate area under PR curve, look for points
+        # where X axis (recall) changes value
+        i = np.where(mrec[1:] != mrec[:-1])[0]
+        # and sum (\Delta recall) * prec
+        ap = np.sum((mrec[i + 1] - mrec[i]) * mpre[i + 1])
+    return ap
 
 def generate_all_default_boxs():
     all_default_boxes = []
